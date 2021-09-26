a3adbc538494e81dd7fbf386ecc68ab44ac1aa6d
Zapan Gao <g0ne150@hotmail.com>
2021-09-26 19:46:14 +0800

    Update view

--- src/components/TinySqlEditor/Editor/Editor.vue
+++ src/components/TinySqlEditor/Editor/Editor.vue
@@ -5,11 +5,37 @@ import { ref } from "vue";
 const props = defineProps<{ sql?: string }>()
 
 const currentText = ref(props.sql || "")
+const lines = [
+    {
+        text: "select * from xxx;",
+        tokens: [
+            { text: "select", className: "text-blue-700" },
+            { text: " * ", className: "" },
+            { text: "from", className: "text-blue-700" },
+            { text: " xxx", className: "" },
+            { text: ";", className: "" },
+        ]
+    },
+    {
+        text: "select * from xxx;",
+        tokens: [
+            { text: "select", className: "text-blue-700" },
+            { text: " * ", className: "" },
+            { text: "from", className: "text-blue-700" },
+            { text: " xxx", className: "" },
+            { text: ";", className: "" },
+        ]
+    }
+]
 </script>
 
 <template>
     <div class="container mx-auto h-80 overflow-auto">
-        <div class="min-h-full cursor-text font-mono">{{ currentText }}</div>
+        <div class="min-h-full cursor-text font-mono">
+            <div class="view-line" v-for="line in lines">
+                <span v-for="token in line.tokens" :class="token.className">{{ token.text }}</span>
+            </div>
+        </div>
     </div>
 </template>
 
