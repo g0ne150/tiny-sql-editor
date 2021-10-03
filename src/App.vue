724f881d6e7e6e1800f976bc0dbaf01f08680d0f
Zapan Gao <g0ne150@hotmail.com>
2021-10-03 09:23:16 +0800

    Update initial sql

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import TinySqlEditor from "./components/TinySqlEditor/TinySqlEditor.vue";
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
 const sql = `-- 支持中文
+insert into employee (name, level) values('Adam', 10)
 select * from employee;
 
 
